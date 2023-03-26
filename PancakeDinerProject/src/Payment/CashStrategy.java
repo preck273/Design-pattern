3eee5266ed838c2661ec6dbcdc2663db363d65b5
ViragSzabo <szabo.viragnak@gmail.com>
2023-03-26 20:14:23 +0200

    Small changes?

--- PancakeDinerProject/src/Payment/CashStrategy.java
+++ PancakeDinerProject/src/Payment/CashStrategy.java
@@ -1,11 +1,5 @@
 package Payment;
 
 public class CashStrategy implements PaymentStrategy{
-
     @Override
-    public void pay(double value) {
-        System.out.println("Paying $" + value + " in cash");
-
-    }
-}
-
+    public void pay(double value) { System.out.println("Paying $" + value + " in cash"); } }
